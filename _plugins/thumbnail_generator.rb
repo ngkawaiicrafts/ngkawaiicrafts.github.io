5b64eb2d1ae4ca58705503eccb8196a988cf61cf
Andrei Grigorev <andrey.grigoriev@gmail.com>
2023-01-21 18:55:08 -0800

    Lampwork

--- _plugins/thumbnail_generator.rb
+++ _plugins/thumbnail_generator.rb
@@ -2,31 +2,20 @@ require 'rmagick'
 require 'fileutils'
 include Magick
 
-module Jekyll
-    class CustomImage < Liquid::Tag
-        def initialize(tag_name, text, tokens)
-            super
-            @text = text
-        end
-
-        def render(context)
-            "<img src=\"#{context[@markup.strip]}\">"
-        end
-    end
-
-    class ThumbnailGenerator < Generator
+module Ngkc
+    class Generator < Jekyll::Generator
         def generate(site)
             sizes = [320, 640, 1280]
 
-            pages = Hash["drawings" => "index.html"] # "sculptures" => "sculptures.html"
+            pages = Hash["drawings" => "index.html", "sculptures" => "sculptures.html", "lampwork" => "lampwork.html"]
 
             intcol = site.collections.select { |k,v| pages.has_key? k }
             intcol.each do |name, collection|
                 page = site.pages.find { |pg| pg.name == pages[name] }
                 file_infos = []
-                # print "'", name, "'\n"
+                #print "'", name, "'\n"
                 collection.files.each do |file|
-                    # print "  ", file.path, "\n"
+                    #print "  ", file.path, "\n"
                     tld = "assets/#{name}"
                     Dir.mkdir tld unless File.exists? tld
                     dir = "assets/#{name}/#{file.basename}"
@@ -58,7 +47,7 @@ module Jekyll
                         new_files.concat [smallerfn]
                     end
 
-                    reader = StaticFileReader.new(site, dir)
+                    reader = Jekyll::StaticFileReader.new(site, dir)
                     static_files = reader.read(new_files)
                     # static_files.each { |f| f.foo = "bar" }
                     site.static_files.concat(static_files)
@@ -79,7 +68,4 @@ module Jekyll
             "name: #{@name}, width: #{@width}, height: #{@height}"
         end
     end
-
 end
-
-Liquid::Template.register_tag('image', Jekyll::CustomImage)
